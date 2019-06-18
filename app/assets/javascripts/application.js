ae15c9c337e4e2d7e682f671d62b80437f1f767f
tooneaa <tooneaa@hotmail.com>
2019-06-18 21:38:04 +0800

    added flash message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,9 @@
 
 $(document).on('turbolinks:load', function waitloading() {
     $('.ui.dropdown').dropdown();
+
+    $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  })
+;
 })
